0f956713b2ce2e86b66dcac30ce9f304522610d6
Viraj-prodigy-Task <virajjoshi1319@gmail.com>
2024-09-16 00:05:43 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// JavaScript to handle scroll and hover effects
+// document.addEventListener('scroll', function() {
+//     const navbar = document.getElementById('navbar');
+//     if (window.scrollY > 50) {
+//         navbar.classList.add('scrolled');
+//     } else {
+//         navbar.classList.remove('scrolled');
+//     }
+// });
